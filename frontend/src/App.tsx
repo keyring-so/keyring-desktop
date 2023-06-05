f651110eb7b38bbda0d7478f88d274eff0ae8a2f
kaichaosun <kaichaosuna@gmail.com>
2023-06-05 22:58:36 +0800

    remove unused styles and assets.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,33 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet, Transfer} from "../wailsjs/go/main/App";
+import { useState } from "react";
+import { Transfer } from "../wailsjs/go/main/App";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+  const [txId, setTxId] = useState("");
+  const updateTxId = (txId: string) => setTxId(txId);
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
-
-    function transfer() {
-        Transfer("", "ETH", "0xaac042fc227cf5d12f7f532bd27361d5634c06a7", "0x7fd0030d3d21d17fb4056de319fad67a853b3c20", "0.005")
-            .then(updateResultText)
-            .catch(err => updateResultText(err));
-    }
-
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
-            <h1 className="text-3xl fond-bold underline">
-                Keyring Wallet
-            </h1>
-            <div className="input-box">
-                <br />
-                <button className="btn" onClick={transfer}>Transfer</button>
-            </div>
-        </div>
+  function transfer() {
+    Transfer(
+      "",
+      "ETH",
+      "0xaac042fc227cf5d12f7f532bd27361d5634c06a7",
+      "0x7fd0030d3d21d17fb4056de319fad67a853b3c20",
+      "0.005"
     )
+      .then(updateTxId)
+      .catch((err) => updateTxId(err));
+  }
+
+  return (
+    <div>
+      <h1 className="text-3xl fond-bold underline">Keyring Wallet</h1>
+
+      <button className="btn" onClick={transfer}>
+        Transfer
+      </button>
+
+      <div>{txId}</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
