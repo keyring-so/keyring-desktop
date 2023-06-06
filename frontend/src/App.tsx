26f046667a73da092ee6d4b5b306de27cd67f8c9
kaichaosun <kaichaosuna@gmail.com>
2023-06-06 17:07:24 +0800

    transfer eth to some addr

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,52 +6,81 @@ import { Input } from "./components/ui/input";
 
 function App() {
   const [txId, setTxId] = useState("");
+  const [ledger, setLedger] = useState("");
+  const [toAddr, setToAddr] = useState("");
+  const [amount, setAmount] = useState("");
+
   const updateTxId = (txId: string) => setTxId(txId);
 
-  function transfer() {
+  const updateToAddr = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setToAddr(event.target.value);
+  };
+
+  const updateAmount = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(event.target.value);
+  };
+
+  const transfer = () => {
     Transfer(
       "",
-      "ETH",
+      ledger,
       "0xaac042fc227cf5d12f7f532bd27361d5634c06a7",
-      "0x7fd0030d3d21d17fb4056de319fad67a853b3c20",
-      "0.005"
+      toAddr,
+      amount
     )
       .then(updateTxId)
       .catch((err) => updateTxId(err));
-  }
+  };
 
-  function receive() {
+  const receive = () => {
     console.log("receive");
-  }
+  };
 
   return (
-    <div className="m-10">
-      <h1 className="text-3xl fond-bold">Keyring Wallet</h1>
-      <label>To</label>
-      <Input />
-      <label>Amount</label>
-      <Input />
+    <div className="m-10 flex flex-col gap-10">
+      <h1 className="text-3xl fond-bold text-center">Keyring Wallet</h1>
 
-      <Button variant="outline" onClick={transfer}>
-        Transfer
-      </Button>
+      <div className="flex flex-row gap-10">
+        {/* sidebar of different coins */}
+        <div>
+          <h2 className="text-2xl">Ledgers</h2>
 
-      <Button variant="outline" onClick={receive}>
-        Receive
-      </Button>
+          <div className="mt-10 grid grid-cols-2 gap-4">
+            <Button onClick={() => setLedger("BTC")}>Bitcoin</Button>
+            <Button onClick={() => setLedger("ETH")}>Ethereum</Button>
+            <Button onClick={() => setLedger("MATIC")}>Polygon</Button>
+          </div>
+        </div>
 
-      <div>{txId}</div>
+        {/* Assets list, send and receive functions */}
+        <div>
+          <h2>{ledger}</h2>
+          <label>To</label>
+          <Input onChange={updateToAddr} />
+          <label>Amount</label>
+          <Input onChange={updateAmount} />
 
-      <Tabs defaultValue="account" className="w-[400px]">
-        <TabsList>
-          <TabsTrigger value="assets">Assets</TabsTrigger>
-          <TabsTrigger value="records">Records</TabsTrigger>
-        </TabsList>
-        <TabsContent value="assets">
-          Show assets here.
-        </TabsContent>
-        <TabsContent value="records">Show records here.</TabsContent>
-      </Tabs>
+          <Button variant="outline" onClick={transfer}>
+            Send
+          </Button>
+          <Button variant="outline" onClick={receive}>
+            Receive
+          </Button>
+
+          <div>{txId}</div>
+
+          <div>
+            <Tabs defaultValue="account" className="w-[400px]">
+              <TabsList>
+                <TabsTrigger value="assets">Assets</TabsTrigger>
+                <TabsTrigger value="records">Records</TabsTrigger>
+              </TabsList>
+              <TabsContent value="assets">Show assets here.</TabsContent>
+              <TabsContent value="records">Show records here.</TabsContent>
+            </Tabs>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
