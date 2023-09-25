7ce262351d39e024c1ee426d83491e3168de8a39
kaichaosun <kaichaosuna@gmail.com>
2023-09-25 15:06:33 +0800

    remove inter, it broken the refresh in wallet page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -54,7 +54,7 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["Inter var", ...fontFamily.sans],
+        sans: [...fontFamily.sans],
       },
       keyframes: {
         "accordion-down": {
