5fab886a332f563ab99920c9105c912936a64c8f
kaichao <kaichaosuna@gmail.com>
2024-03-29 16:16:46 +0800

    fix sidebar tooltip (#94)

--- frontend/src/components/sidebar-ledger.tsx
+++ frontend/src/components/sidebar-ledger.tsx
@@ -1,8 +1,14 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
 import { ledgerAtom, showSidebarItem } from "@/store/state";
 import { useSetAtom } from "jotai";
 import { LucideIcon } from "lucide-react";
 import React from "react";
-import { cn } from "@/lib/utils";
 
 type Props = {
   img?: string;
@@ -19,7 +25,7 @@ const SidebarLedger = ({ img, icon, text, ledger, selected }: Props) => {
   return (
     <div
       className={cn(
-        "relative flex items-center justify-center h-14 w-14 mt-2 mb-2 mx-auto bg-gray-100 hover:bg-primary dark:bg-gray-800 text-zinc-600 hover:text-white hover:rounded-xl rounded-full transition-all duration-200 ease-linear cursor-pointer shadow-lg group",
+        "relative h-14 w-14 mt-2 mb-2 bg-gray-100 hover:bg-primary dark:bg-gray-800 text-zinc-600 hover:text-white hover:rounded-xl rounded-full transition-all duration-200 ease-linear cursor-pointer shadow-lg",
         selected ? "bg-primary rounded-xl" : ""
       )}
       onClick={() => {
@@ -27,17 +33,19 @@ const SidebarLedger = ({ img, icon, text, ledger, selected }: Props) => {
         setShowSidebarItem("ledger");
       }}
     >
-      {icon ? React.createElement(icon) : <img src={img} />}
-      <span
-        className="
-            fixed w-auto p-2 m-2 min-w-max left-16 rounded-md shadow-md
-            text-white bg-primary
-            text-sm font-bold
-            transition-all duration-300 scale-0 origin-left group-hover:scale-100
-            "
-      >
-        {text}
-      </span>
+      <TooltipProvider delayDuration={100}>
+        <Tooltip>
+          <TooltipTrigger>
+            {icon ? React.createElement(icon) : <img src={img} />}
+          </TooltipTrigger>
+          <TooltipContent
+            side="left"
+            className="bg-primary text-secondary font-bold ml-2"
+          >
+            <p>{text}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </div>
   );
 };
