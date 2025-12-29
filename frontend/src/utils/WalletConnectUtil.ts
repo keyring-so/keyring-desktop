080dc3a057459728aff6dbd17269d25ad8cd4817
kaichaosun <kaichaosuna@gmail.com>
2025-12-29 16:20:01 +0800

    remove unused

--- frontend/src/utils/WalletConnectUtil.ts
+++ frontend/src/utils/WalletConnectUtil.ts
@@ -2,19 +2,8 @@
 // import { Core } from "@walletconnect/core";
 import { GetWalletConnectProjectId } from "@/../wailsjs/go/main/App";
 
-import { Core } from '@walletconnect/core'
-import { WalletKit, IWalletKit } from '@reown/walletkit'
-
-const core = new Core({
-  projectId: '0a2083f8fd723dc761a50a53c0965a49'
-})
-
-const metadata = {
-  name: 'Keyring',
-  description: 'Secure and handy hardware wallet for crypto holders.',
-  url: 'https://keyring.so',
-  icons: ['https://imagedelivery.net/_aTEfDRm7z3tKgu9JhfeKA/0ba975d7-08d7-4a71-d527-bc7d54bad600/sm']
-}
+import { Core } from "@walletconnect/core";
+import { WalletKit, IWalletKit } from "@reown/walletkit";
 
 export let web3wallet: IWalletKit;
 
@@ -30,7 +19,9 @@ export const createWeb3Wallet = async () => {
       name: "Keyring",
       description: "Secure and handy hardware wallet for crypto holders",
       url: "https://keyring.so",
-      icons: ['https://imagedelivery.net/_aTEfDRm7z3tKgu9JhfeKA/0ba975d7-08d7-4a71-d527-bc7d54bad600/sm'],
+      icons: [
+        "https://imagedelivery.net/_aTEfDRm7z3tKgu9JhfeKA/0ba975d7-08d7-4a71-d527-bc7d54bad600/sm",
+      ],
     },
   });
   console.log("web3wallet inited", web3wallet);
